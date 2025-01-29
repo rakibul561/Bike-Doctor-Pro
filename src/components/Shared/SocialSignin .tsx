469bd61a2aa445da59db2ef0f32ebc36551b9a24
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-01-29 07:27:53 -0800

    google login is ohk

--- src/components/Shared/SocialSignin .tsx
+++ src/components/Shared/SocialSignin .tsx
@@ -8,11 +8,13 @@ import { BsGithub, BsGoogle } from "react-icons/bs";
 const SocialSignin:React.FC = () => {
    
   const handleSignin = async (provider: string) =>{
+    console.log(provider);
+    
   
     // const resp = await signIn(provider)
     const resp = await signIn(provider)
   console.log(resp);
-   
+    
   } 
   
  
@@ -20,7 +22,7 @@ const SocialSignin:React.FC = () => {
   return (
     <div className="flex items-center justify-center space-x-3">
       <button  className="btn  flex items-center justify-center text-green-500">
-        <BsGoogle  onClick={() =>handleSignin('goole')}/>
+        <BsGoogle  onClick={() =>handleSignin('google')}/>
       </button>
 
       <button  className="btn  flex items-center justify-center text-primary">
