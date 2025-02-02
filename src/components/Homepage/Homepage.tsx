f3059b1b402d66bd88176c9bd7cdbd7e8907dcea
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-02 08:47:48 -0800

    our team is ohk

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import About from './About';
 import Services from './Services';
+import Team from './Team';
 
 const Homepage = () => {
     return (
@@ -9,6 +10,7 @@ const Homepage = () => {
           <Banner/>
           <About/>
           <Services/>
+          <Team/>
             
         </div>
     );
---
+++ src/components/Homepage/Team.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import React from "react";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+
+const Team = () => {
+  return (
+    <div className="container mx-auto mb-8">
+      <div className="text-center container mx-auto">
+        <h3 className="text-2xl font-bold text-orange-600">Team</h3>
+        <h2 className="text-5xl">Meet Our Team</h2>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised <br /> words which do not look even slightly
+          believable.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/1.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/2.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/3.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
