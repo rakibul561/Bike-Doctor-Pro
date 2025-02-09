bb7d7fe0068d8192d26f06b0470a17fa22ab4bd9
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-09 08:04:20 -0800

    complate contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,18 +4,18 @@ import Image from 'next/image'; // Next.js Image component for optimization
 
 const Page: React.FC = () => {
   return (
-    <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
+    <div className="grid border lg:mt-10 lg:mb-10  max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-2xl md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
       <div className="flex flex-col justify-between">
-        <div className="space-y-2">
+        <div className="space-y-2"> 
           <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Lets talk!</h2>
           <div className="dark:text-gray-600">Vivamus in nisl metus? Phasellus.</div>
         </div>
-        <Image src="/assets/svg/doodle.svg" alt="Doodle" className="p-6 h-52 md:h-64" width={400} height={400} />
+        <Image src={"/assets/images/team/2.jpg"} alt="Doodle" className=" rounded-lg h-52 md:h-64" width={400} height={400} />
       </div>
       <form noValidate className="space-y-6">
         <div>
           <label htmlFor="name" className="text-sm">Full name</label>
-          <input id="name" type="text" placeholder="Your full name" className="w-full p-3 rounded dark:bg-gray-100" />
+          <input id="name" type="text" placeholder="Your full name" className="w-full p-3 rounded dark:bg-gray-100 border" />
         </div>
         <div>
           <label htmlFor="email" className="text-sm">Email</label>
@@ -23,7 +23,7 @@ const Page: React.FC = () => {
             id="email"
             type="email"
             placeholder="Your email"
-            className="w-full p-3 rounded dark:bg-gray-100"
+            className="w-full p-3 rounded border dark:bg-gray-100"
             style={{
               backgroundImage: 'url("data:image/png;base64,iVBORw0KGgo...")',
               backgroundRepeat: 'no-repeat',
@@ -34,9 +34,9 @@ const Page: React.FC = () => {
         </div>
         <div>
           <label htmlFor="message" className="text-sm">Message</label>
-          <textarea id="message" rows={3} className="w-full p-3 rounded dark:bg-gray-100" placeholder="Your message"></textarea>
+          <textarea id="message" rows={3} className="w-full p-3 border rounded dark:bg-gray-100" placeholder="Your message"></textarea>
         </div>
-        <button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-600 dark:text-gray-50">
+        <button type="submit" className="btn btn-primary  ">
           Send Message
         </button>
       </form>
