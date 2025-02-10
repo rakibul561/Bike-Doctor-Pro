ea70d94dc3313dbac8e68d434dbbb1485d434b2f
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-10 09:34:51 -0800

    update code

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,9 +5,10 @@ const page = () => {
 
         <div>
             this is a blog page 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet aliquid iure provident voluptas itaque pariatur culpa laboriosam ipsum animi eius ad corrupti dignissimos numquam, possimus iste excepturi eveniet earum illum asperiores ratione magni. Non, accusantium pariatur. Eveniet eum veniam dolorum cupiditate dolores tenetur tempore, adipisci iusto id laudantium dignissimos ea laborum beatae. Dignissimos quasi doloremque, iusto temporibus sit eum molestias esse corporis expedita illum, eius, iste ratione tenetur odio? Dolore, possimus laudantium! Rem sapiente ad, dignissimos laborum ab nobis suscipit nam reprehenderit? Perferendis quia velit animi at distinctio maiores, nisi a dolores provident numquam expedita doloribus ratione! Nemo, optio!
             
         </div>
-        
+
     );
 };
 
