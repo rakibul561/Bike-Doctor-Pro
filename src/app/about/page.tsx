62c82c3d24cd143a766d24bcc02c6b555ef404ab
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-13 06:31:02 -0800

    about page is ohk

---
+++ src/app/about/page.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Aboutpage:React.FC = () => {
+    return (
+        <div>
+        
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto">
+        <div className="relative lg:w-1/2">
+          <img
+            src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+            
+          />
+          <svg
+            className="absolute top-0 right-0 hidden h-full text-white lg:inline-block"
+            viewBox="0 0 20 104"
+            fill="currentColor"
+          >
+            <polygon points="17.3036738 5.68434189e-14 20 5.68434189e-14 20 104 0.824555778 104" />
+          </svg>
+        </div>
+        <div className="flex flex-col justify-center p-8 bg-white lg:p-16 lg:pl-10 lg:w-1/2">
+          <div>
+            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+              Brand new
+            </p>
+          </div>
+          <h5 className="mb-3 text-3xl font-extrabold leading-none sm:text-4xl">
+            Your new ideal style
+          </h5>
+          <p className="mb-5 text-gray-800">
+            <span className="font-bold">Lorem ipsum</span> dolor sit amet,
+            consectetur adipiscing elit. Etiam sem neque, molestie sit amet
+            venenatis et, dignissim ut erat. Sed aliquet velit id dui eleifend,
+            sed consequat odio sollicitudin.
+          </p>
+          <div className="flex items-center">
+            <button
+              type="submit"
+              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide bg-primary text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+            >
+              Get started
+            </button>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+        </div>
+    );
+};
+
+export default Aboutpage;
