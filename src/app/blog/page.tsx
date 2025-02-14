98d574ec1ee6b192d253fbc5a7174dab91f8d815
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-14 02:05:21 -0800

    update a code

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -125,6 +125,102 @@ const page = () => {
               Learn more
             </Link>
           </div>
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <Link
+                href="/"
+                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+                aria-label="Category"
+                title="traveling"
+              >
+                traveling
+              </Link>
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+            <Link
+              href="/"
+              aria-label="Category"
+              title="Film It!"
+              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              Film It!
+            </Link>
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <Link
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </Link>
+          </div>
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <Link
+                href="/"
+                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+                aria-label="Category"
+                title="traveling"
+              >
+                traveling
+              </Link>
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+            <Link
+              href="/"
+              aria-label="Category"
+              title="Film It!"
+              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              Film It!
+            </Link>
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <Link
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </Link>
+          </div>
+          <div className="p-5 border border-t-0">
+            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+              <Link
+                href="/"
+                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+                aria-label="Category"
+                title="traveling"
+              >
+                traveling
+              </Link>
+              <span className="text-gray-600">— 28 Dec 2020</span>
+            </p>
+            <Link
+              href="/"
+              aria-label="Category"
+              title="Film It!"
+              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              Film It!
+            </Link>
+            <p className="mb-2 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <Link
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </Link>
+          </div>
         </div>
       </div>
     </div>
