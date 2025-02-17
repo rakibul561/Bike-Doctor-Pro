47900f049a2d860aa62afe731e0abcef5f773f7f
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-17 06:00:06 -0800

    update a code

--- src/components/lib/product.ts
+++ src/components/lib/product.ts
@@ -162,42 +162,7 @@ export const product = [
           "description": "Long-lasting car battery for consistent power supply.",
           "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
         },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
+       
       ]
       
 ]
