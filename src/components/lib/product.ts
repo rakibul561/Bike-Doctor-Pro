7f71ea5be7f645b11277cab47dffab1a95527be8
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-18 08:09:59 -0800

    update code

--- src/components/lib/product.ts
+++ src/components/lib/product.ts
@@ -144,18 +144,7 @@ export const product = [
           "description": "Long-lasting car battery for consistent power supply.",
           "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
         },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
-        {
-          "name": "Car Battery",
-          "price": "20.00",
-          "description": "Long-lasting car battery for consistent power supply.",
-          "img": "https://i.ibb.co/fDbYZyV/car-battery.jpg"
-        },
+      
         {
           "name": "Car Battery",
           "price": "20.00",
