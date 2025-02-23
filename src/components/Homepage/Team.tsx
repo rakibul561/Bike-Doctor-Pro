eab4e234a2931bbf7b1229d185990f3dded2e5c6
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-23 05:04:58 -0800

    meet our team updated

--- src/components/Homepage/Team.tsx
+++ src/components/Homepage/Team.tsx
@@ -67,6 +67,108 @@ const Team = () => {
             <FaInstagram className="text-3xl"/>
             </div>
         </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/2.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/3.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/2.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/3.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/2.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
+        <div className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+         <Image  width={640} height={ 640} src={"/assets/images/team/3.jpg"} alt="iamge"/>
+          <div className="mt-6 mb-2">
+            <h2 className="text-xl text-center font-semibold tracking-wide">
+            Car Engine Plug
+            </h2>
+          </div>
+          <h2 className="text-center  tracking-wide">
+          Engine Expert
+            </h2>
+            <div className="flex text-center justify-center gap-4 mt-2">
+            <FaFacebook className="text-3xl"/>
+            <FaTwitter className="text-3xl"/>
+            <FaLinkedin className="text-3xl"/>
+            <FaInstagram className="text-3xl"/>
+            </div>
+        </div>
       </div>
     </div>
   );
