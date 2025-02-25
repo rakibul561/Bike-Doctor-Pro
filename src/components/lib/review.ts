48c3106faa0e6abe7787c988333c6824793f1da4
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-02-25 09:52:52 -0800

    change this code

--- src/components/lib/review.ts
+++ src/components/lib/review.ts
@@ -3,7 +3,7 @@ export const reviews = [
       "id": 1,
       "name": "Awlad Hossain",
       "profession": "Businessman",
-      "review": "Amazing service! They repaired my car perfectly and the pricing was very reasonable. Highly recommended.",
+      "comment": "Amazing service! They repaired my car perfectly and the pricing was very reasonable. Highly recommended.",
       "rating": 5,
       "image": "https://i.ibb.co/d0vV175Y/empowered-business-woman-working-city-23-2149589068.jpg"
     },
@@ -11,7 +11,7 @@ export const reviews = [
       "id": 2,
       "name": "Sarah Karim",
       "profession": "Software Engineer",
-      "review": "I bought some bike accessories from here, and the quality is top-notch. Fast delivery as well!",
+      "comment": "I bought some bike accessories from here, and the quality is top-notch. Fast delivery as well!",
       "rating": 4.5,
       "image": "https://i.ibb.co/9mYqHckC/stylish-man-looking-notepad-1301-1758.jpg"
     },
@@ -19,7 +19,7 @@ export const reviews = [
       "id": 3,
       "name": "John Doe",
       "profession": "Entrepreneur",
-      "review": "Excellent repair service! My bike was fixed quickly, and it runs like new now. Great experience!",
+      "comment": "Excellent repair service! My bike was fixed quickly, and it runs like new now. Great experience!",
       "rating": 5,
       "image": "https://i.ibb.co/ccxp41CC/handsome-young-man-using-his-digital-tablet-isolated-white-1301-4938.jpg"
     },
@@ -27,7 +27,7 @@ export const reviews = [
       "id": 4,
       "name": "Rafiq Rahman",
       "profession": "Driver",
-      "review": "Affordable and reliable service. They fixed my car's engine problem within a day. Highly professional team.",
+      "comment": "Affordable and reliable service. They fixed my car's engine problem within a day. Highly professional team.",
       "rating": 4.8,
       "image": "https://i.ibb.co/qFFx9xbY/portrait-confident-young-woman-with-eyeglasses-23-2148452693.jpg"
     },
